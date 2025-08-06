9943b33dae30a6c4b2508fe20421e4f3fb512db6
govind <govind.corp.trainer@gmail.com>
2025-08-06 21:03:49 +0530

    changes to 5

--- src/main/java/com/cg/service/EBService.java
+++ src/main/java/com/cg/service/EBService.java
@@ -5,7 +5,7 @@ public class EBService {
 	public double calcBill(double units){
 		double bill = 0;
 		if(units > 200)
-			bill = 100 * 1 + 100 * 2 + (units - 200) * 4;
+			bill = 100 * 1 + 100 * 2 + (units - 200) * 5;
 		else if(units > 100)
 			bill = 100 * 1 + (units - 100) * 2.7;
 		else
