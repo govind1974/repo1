ed36101d2825ea64e63bb6844c0c0678116ea71f
govind <govind.corp.trainer@gmail.com>
2025-08-07 17:00:11 +0530

    changed validation of name having min 3 and max 3 chars

--- src/main/java/com/cg/service/PalindromeService.java
+++ src/main/java/com/cg/service/PalindromeService.java
@@ -3,7 +3,7 @@ package com.cg.service;
 public class PalindromeService {
 	
 	public boolean isPalindrome(String str){
-		if(!str.matches("[a-zA-Z]{2,}"))
+		if(!str.matches("[a-zA-Z]{2,10}"))
 			return false;
 		StringBuffer sb = new StringBuffer(str);
 		String rev = sb.reverse().toString();
